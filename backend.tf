1e832047477446725473373c5b7ec456edba1359
pmaddiralla <pmaddiralla@apple.com>
2026-03-20 15:40:25 +0530

    stage updated

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "prasad-s3-demo-xyz-1234" # change this
-    key            = "prasad/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"   
-    
-  }
-}
---
+++ stage.tf
@@ -0,0 +1,8 @@
+cidr = "10.1.0.0/16"
+envname = "stage"
+region = "ap-southeast-1"
+pubsubnet = ["10.1.0.0/24","10.1.1.0/24"]
+privatesubnet = ["10.1.3.0/24","10.1.4.0/24"]
+azs = ["ap-southeast-1a","ap-southeast-1b"]
+ami = "ami-0be9cb9f67c8dabd6"
+type = "t2.micro" 
